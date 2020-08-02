e7da1da6ff285fe6fbbf1eade127273dce19bb30
unknown <duan.manosso@hotmail.com>
2020-08-02 19:30:33 -0300

    implementação forms

--- src/Pages/cadastro/Categoria/index.js
+++ src/Pages/cadastro/Categoria/index.js
@@ -1,16 +1,69 @@
-import React from 'react'
+import React, { useState } from 'react'
 import PageDefault from '../../../componentes/PageDefault'
 import { Link } from 'react-router-dom'
+import FormField from '../../../componentes/FormField';
 
+function CadastroCategoria() {
+  const valoresIniciais = {
+    nome: '',
+    descricao: '',
+    cor: ''
+  }
+  const [categorias, setCategorias] = useState([]);
+  const [valores, setValores] = useState(valoresIniciais);
+  
 
-function CategoriaVideo() {
-    return (
+  function setValor(chave, valor) {
+    setValores({
+      ...valores,
+      [chave]: valor, //nome: 'valor'
+    })
+  }
+
+  function handleChange(infosDoEvento) { 
+    setValor(
+      infosDoEvento.target.getAttribute('name'), 
+      infosDoEvento.target.value
+    )
+  }
+  
+  return (
       <PageDefault>
-        <h1>Cadastro de Categoria</h1>
-        <form>
-          <label>Nome da Categoria:
-            <input type="text" />
-          </label>
+        <h1>Cadastro de Categoria: {valores.nome}</h1>
+        
+        <form onSubmit={function handleSubmit(infosDoEvento) {
+          infosDoEvento.preventDefault();
+          setCategorias([
+            ...categorias,
+            valores
+          ]);
+
+          setValores(valoresIniciais)
+          }}>
+
+          <FormField 
+            label="Nome da Categoria"
+            type="text"
+            name="nome"
+            value={valores.nome}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Descrição"
+            type="text"
+            name="descricao"
+            value={valores.descricao}
+            onChange={handleChange}
+          />
+
+          <FormField
+            label="Cor"
+            type="color"
+            name="cor"
+            value={valores.cor}
+            onChange={handleChange}
+          />
           
           <button> 
             Cadastrar
@@ -18,6 +71,16 @@ function CategoriaVideo() {
         </form>
 
 
+        <ul>
+          {categorias.map((categoria, indice) => {
+            return(
+              <li key={`${categoria}${indice}`}>
+                {categoria.nome}
+              </li>
+            )
+          })}
+        </ul>
+          
 
         <Link to="/">
             Ir para home
@@ -26,4 +89,4 @@ function CategoriaVideo() {
     )
   };
 
-  export default CategoriaVideo
+  export default CadastroCategoria
--- src/index.js
+++ src/index.js
@@ -5,14 +5,14 @@ import Home from './Pages/HomePage';
 
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CadastroVideo from './Pages/cadastro/Video';
-import CategoriaVideo from './Pages/cadastro/Categoria';
+import CadastroCategoria from './Pages/cadastro/Categoria';
 
 ReactDOM.render(
   <BrowserRouter>
     <Switch>
       <Route path="/" component={Home} exact/>
       <Route path="/cadastro/video" component={CadastroVideo} />
-      <Route path="/cadastro/categoria" component={CategoriaVideo} />
+      <Route path="/cadastro/categoria" component={CadastroCategoria} />
       <Route component={() => (<div>ERRO 404</div>)} />
     </Switch>
   </BrowserRouter>,
